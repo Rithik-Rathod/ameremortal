a0f15a14c2ae97518e8406861f5edad9476bc0ea
ameremortal <169123458+ameremortal@users.noreply.github.com>
2024-05-21 07:23:07 +0530

    Update faces.py

--- Python_CS50_2022/PS_0/faces.py
+++ Python_CS50_2022/PS_0/faces.py
@@ -1,4 +1,6 @@
+#We'll convert emojis into emoticons.
 x=input(" I'll convert magically your emoticons to emojis")
+#The emoticons were obtained from the PS using inspector element.
 x=x.replace(":(","🙁")
 x=x.replace(":)","🙂")
 print(x)

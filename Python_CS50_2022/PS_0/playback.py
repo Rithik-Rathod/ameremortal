0a16be24ad2d7aeff7e9e3d2cbcb790eba9b3e31
ameremortal <169123458+ameremortal@users.noreply.github.com>
2024-05-21 07:25:59 +0530

    Update playback.py

--- Python_CS50_2022/PS_0/playback.py
+++ Python_CS50_2022/PS_0/playback.py
@@ -1,4 +1,7 @@
+#Here, we are adding "..." between words
 mol=input("What's the meaning of life?")
+#Default of .split() is spacing so we dont need to do shit there
 mol=mol.split()
+# we are using .join(mol) to join the mol which we split above
 y= "...".join(mol)
 print(y)
